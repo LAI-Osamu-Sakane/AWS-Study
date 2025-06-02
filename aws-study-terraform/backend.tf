bd2519d918b454b16f0b19f43bb39b0f5fedd3e6
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2025-06-02 18:14:57 +0900

    test用赤3

--- aws-study-terraform/backend.tf
+++ aws-study-terraform/backend.tf
@@ -1,11 +1,11 @@
 # tfstare管理バケット
 terraform {
-    required_version = "=1.11.3"
-    backend "s3" {
-      bucket = "aws-study-test-01"
-      key = "aws-study-test-01"
-      region = "ap-northeast-1"
-    }
+  required_version = "=1.11.3"
+  backend "s3" {
+    bucket = "aws-study-test-01"
+    key    = "aws-study-test-01"
+    region = "ap-northeast-1"
+  }
 }
 
 # プロバイダ-指定
--- aws-study-terraform/cloudwatchalarm.tf
+++ aws-study-terraform/cloudwatchalarm.tf
@@ -1,14 +1,14 @@
 resource "aws_cloudwatch_metric_alarm" "foobar" {
-  alarm_name                = "AWS-study-EC2CPUutilizationAlarm"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 1
+  alarm_name          = "AWS-study-EC2CPUutilizationAlarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
   datapoints_to_alarm = 1
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  period                    = 300
-  statistic                 = "Average"
-  threshold                 = 15
-  unit = "Percent"
-  alarm_description         = "This metric monitors ec2 cpu utilization"
-  treat_missing_data = "missing"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 15
+  unit                = "Percent"
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  treat_missing_data  = "missing"
 }
