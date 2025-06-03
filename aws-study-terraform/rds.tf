d0ff1c11c7cbf1108981400e38afe46f6bb0181c
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2025-06-03 15:13:39 +0900

    test8

--- aws-study-terraform/rds.tf
+++ aws-study-terraform/rds.tf
@@ -26,7 +26,7 @@ resource "aws_db_instance" "test-rds" {
   vpc_security_group_ids      = [aws_security_group.sg-rds.id]
   db_subnet_group_name        = aws_db_subnet_group.rds-study-test-subnet.name
   # db_subnet_group_name = "rds-private-subnet"
-  skip_final_snapshot  = true
+  skip_final_snapshot = true
   depends_on = [
     aws_db_subnet_group.rds-study-test-subnet
   ]
